4b7f3c0ee2ae67d15777636741827b6a939110a9
ner0eXe <86475942+ner0eXe@users.noreply.github.com>
2025-04-08 13:45:23 +0700

    Update Discipline.h

--- ConsoleApplication2/Discipline.h
+++ ConsoleApplication2/Discipline.h
@@ -1,27 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <time.h>
 #include <sstream>
 #include <iomanip>
-
-Discipline::Discipline(std::string n, int lc, int comp = 1)
-    : name(n), labCount(lc), complexity(comp) {
-
-    if (name.empty()) {
-        throw std::invalid_argument("Name cannot be empty.");
-    }
-
-    if (labCount <= 0) {
-        throw std::invalid_argument("Lab count must be greater than zero.");
-    }
-
-    if (complexity < 1 || complexity > 10) {
-        throw std::out_of_range("Complexity must be in the range [1, 10].");
-    }
-        
-    labGrades = new int[labCount]();
-}
+#include <time.h>
 
 class Discipline {
 private:
@@ -30,11 +12,33 @@ private:
     int* labGrades;
     int complexity;
 
+    static const int DEFAULT_GRADE = -1;
+
 public:
-    Discipline(std::string n, int lc, int comp);
+    Discipline(std::string n, int lc, int comp)
+        : name(n), labCount(lc), complexity(comp) {
 
+        if (name.empty()) {
+            throw std::invalid_argument("Name cannot be empty.");
+        }
 
-    ~Discipline();
+        if (labCount <= 0) {
+            throw std::invalid_argument("Lab count must be greater than zero.");
+        }
+
+        if (complexity < 1 || complexity > 10) {
+            throw std::out_of_range("Complexity must be in the range [1, 10].");
+        }
+
+        labGrades = new int[labCount];
+        for (int i = 0; i < labCount; ++i) {
+            labGrades[i] = DEFAULT_GRADE;
+        }
+    }
+
+    ~Discipline() {
+        delete[] labGrades;
+    }
 
     int& operator[](int index);
 
