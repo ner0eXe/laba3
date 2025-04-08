5e9fc62c101b830a02853109a9c18f7464409755
ner0eXe <86475942+ner0eXe@users.noreply.github.com>
2025-04-08 22:27:16 +0700

    Update Discipline.h

--- ConsoleApplication2/Discipline.h
+++ ConsoleApplication2/Discipline.h
@@ -15,26 +15,7 @@ private:
     static const int DEFAULT_GRADE = -1;
 
 public:
-    Discipline(std::string n, int lc, int comp)
-        : name(n), labCount(lc), complexity(comp) {
-
-        if (name.empty()) {
-            throw std::invalid_argument("Name cannot be empty.");
-        }
-
-        if (labCount <= 0) {
-            throw std::invalid_argument("Lab count must be greater than zero.");
-        }
-
-        if (complexity < 1 || complexity > 10) {
-            throw std::out_of_range("Complexity must be in the range [1, 10].");
-        }
-
-        labGrades = new int[labCount];
-        for (int i = 0; i < labCount; ++i) {
-            labGrades[i] = DEFAULT_GRADE;
-        }
-    }
+    Discipline(std::string n, int lc, int comp);
 
     ~Discipline();
 
